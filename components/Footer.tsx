633e93704f4e23f6c40e0891fb655d8d4f296a12
Matthew Willsmore <100129488+mwillsmore@users.noreply.github.com>
2025-09-12 01:39:17 +0100

    Constrain footer to main content width

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[132px] px-2 flex justify-between items-end">
+    <footer className="w-full max-w-[800px] mx-auto h-[132px] px-2 flex justify-between items-end">
       <p className="text-white text-sm">© 2025 S&W™ Productions. All rights reserved.</p>
       <Image
         src="/SW-productions.png"
